7af7da28be45e317ef0ad6811b8824ced73efbda
Rodrigo T. Guirra <rguirra@apoioprodesp.sp.gov.br>
2026-07-16 19:55:29 -0300

    feat: melhoria no header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,27 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="professor/index"
-        options={{
-          title: "Professor",
-        }}
-      />
-
-      <Stack.Screen
-        name="aluno/index"
-        options={{
-          title: "Aluno",
-        }}
-      />
-
-      <Stack.Screen
-        name="Login/Login"
-        options={{
-          title: "Login",
-        }}
-      />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="professor" />
+      <Stack.Screen name="aluno" />
+      <Stack.Screen name="login" />
     </Stack>
   );
 }
